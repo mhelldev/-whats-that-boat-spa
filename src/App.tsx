0f688595f06e9b50017d249177dfab427b525332
michael heller <michaelheller007@googlemail.com>
2022-08-09 16:23:28 +0200

    query for vessels

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { MapContainer, TileLayer, useMap } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css';
 
 function App() {
+  useEffect(() => {
+
+      const timeframe = [60, 525600];
+      const time = timeframe.join(',');
+      const lat = 51.3023
+      const lng = 6.7355
+      const distance =  2
+      fetch(`/en/reports?asset_type=vessels&columns=time_of_latest_position:desc,
+  flag,shipname,photo,recognized_next_port,reported_eta,reported_destination,current_port,imo,ship_type,show_on_
+  live_map,area,lat_of_latest_position,lon_of_latest_position&time_of_latest_position_between=${time}&near_me=${lat},${lng},${distance}`, {
+          headers: {
+              'vessel-image': '0053e92efe9e7772299d24de2d0985adea14',
+          },
+
+      }).then(res =>
+      {res.json().then((json) => console.log(json))}
+    )
+
+
+  }, [])
+
   return (
     <div className="App">
       <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
